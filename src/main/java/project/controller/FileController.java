9d771f3c96ea0c685448788374a81f2f5d5e0ad2
Anand Raj M <anandraj.murugesan@accoliteindia.com>
2019-01-30 18:45:33 +0530

    added xl

--- src/main/java/project/controller/FileController.java
+++ src/main/java/project/controller/FileController.java
@@ -2,6 +2,7 @@ package project.controller;
 
 import javax.servlet.annotation.MultipartConfig;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import project.model.ResponseObject;
 import project.service.FileService;
 
 @RestController
@@ -19,21 +21,24 @@ public class FileController {
 	@Autowired
 	FileService fileService;
 	
-	@PostMapping("/upload")
-	//@CrossOrigin(origins = "http://localhost:4200") // Call  from Local Angualar
-	public ResponseEntity<?> storeWithService(@RequestParam("testingData") MultipartFile file)
+	@PostMapping(path = "/upload", produces = "application/json")
+	//@CrossOrigin(origins = "http://localhost:4200") // Call  from Local Angular
+	public ResponseObject storeWithService(@RequestParam("testingData") MultipartFile file)
 	{
 		String message = "";
 		try
 		{
 			fileService.storeWithDao(file);
 			message = "You successfully uploaded " + file.getOriginalFilename() + "!";
-			return new ResponseEntity<>(message,HttpStatus.OK);
+			fileService.startAutomation(file);
+			Thread.sleep(5000);
+			return new ResponseObject("success");
 		}
 		catch(Exception e) 
 		{
 			message = "Fail to upload Profile Picture" + file.getOriginalFilename() + "!";
-			return new ResponseEntity<>(message,HttpStatus.EXPECTATION_FAILED);
+			//return new ResponseEntity<>(message,HttpStatus.EXPECTATION_FAILED);
+			return new ResponseObject("Failed");
 		}
 	}
 	
